81241c1e881f51c93bb95a4fab6c14e395c99d23
mezzolite <mez.charney@gmail.com>
2020-01-07 10:44:57 -0700

    adds postgresql, knex, creates db

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const userRoutes = require('./routes/user-routes')
 
 app.listen(port, () => console.log(`listening on port ${port}`))
+
+app.use(userRoutes)

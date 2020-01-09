1a3ee53e79b0a77c524016f1b0fd26fe0fb92119
mezzolite <mez.charney@gmail.com>
2020-01-09 09:57:45 -0700

    fixes avatar image links

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const userRouter = require('./routes/user-routes')
+const avatarRouter = require('./routes/avatar-routes')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 app.listen(port, () => console.log(`listening on port ${port}`))
 
+app.use(cors())
+app.use(bodyParser.json())
+
 app.use(userRouter)
+app.use(avatarRouter)

36cbe91664f55c6455e09ded49a21aef40bec368
mezzolite <mez.charney@gmail.com>
2020-01-23 10:46:40 -0700

    changed app.listen position

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
 
-app.listen(port, () => console.log(`listening on port ${port}`))
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -18,3 +17,5 @@ app.use(userRouter)
 app.use(avatarRouter)
 app.use(actionRouter)
 app.use(userActionsRouter)
+
+app.listen(port, () => console.log(`listening on port ${port}`))
